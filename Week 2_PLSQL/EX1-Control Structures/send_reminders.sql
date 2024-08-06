9992101ba02671140373f4f07cf0438d8e48d8bc
Drisshya S <160033310+drisshya@users.noreply.github.com>
2024-08-06 18:24:51 +0530

    Delete Week 2_PLSQL directory

--- Week 2_PLSQL/EX1-Control Structures/send_reminders.sql
+++
@@ -1,9 +0,0 @@
-BEGIN
-  FOR rec IN (SELECT loan_id, customer_id, due_date
-              FROM loans
-              WHERE due_date BETWEEN SYSDATE AND SYSDATE + 30) 
-  LOOP
-    DBMS_OUTPUT.PUT_LINE('Reminder: Loan ID ' || rec.loan_id || ' for Customer ID ' || rec.customer_id || ' is due on ' || rec.due_date || '. Please make sure to make the payment before the due date.');
-  END LOOP;
-END;
-/
--- Week 2_PLSQL/EX5-Triggers/log_transaction.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER LogTransaction
-AFTER INSERT ON Transactions
-FOR EACH ROW
-BEGIN
-  INSERT INTO AuditLog (transaction_id, transaction_date, customer_id, amount, action)
-  VALUES (:NEW.transaction_id, :NEW.transaction_date, :NEW.customer_id, :NEW.amount, 'INSERT');
-END LogTransaction;
-/
